a52f3bfa36866030b45174c587ffe2c7dc85f79a
Pavel Yatsenko <mirtexniki@ukr.net>
2022-08-07 16:28:13 +0300

    HW_8_3
    Сделал мобильное меню. Вроде все сделал, и все работает на 3х точках перелома.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,63 +1,28 @@
-// (() => {
-//   const refs = {
-//     openMobileMenuBtn: document.querySelector("[data-mobile-menu-open]"),
-//     closeMobileMenuBtn: document.querySelector("[data-mobile-menu-close]"),
-//     mobileMenu: document.querySelector("[data-mobile-menu]"),
-//   };
-
-//   refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
-//   refs.closeMobileMenuBtn.addEventListener("click", toggleMobileMenu);
-
-//   function toggleMobileMenu() {
-//     refs.mobileMenu.classList.toggle("is-hidden");
-//   }
-// })();
-
-
-// ВТОРОЙ ВАРИАНТ
-
 (() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-button]'),
-    closeMenuBtn: document.querySelector('[data-menu-button-close]'),
-    menu: document.querySelector('[data-menu]'),
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
 
-  function toggleModal() {
-    refs.menu.classList.toggle('is-open');
-  }
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 })();
-
-// ТРЕТИЙ ВАРИАНТ
-
-// (() => {
-//   const mobileMenu = document.querySelector("[data-menu]");
-//   const openMenuBtn = document.querySelector("[data-menu-open]");
-//   const closeMenuBtn = document.querySelector("[data-menu-close]");
-
-//   const toggleMenu = () => {
-//     const isMenuOpen =
-//       openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-//     openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
-//     mobileMenu.classList.toggle("is-open");
-
-//     const scrollLockMethod = !isMenuOpen
-//       ? "disableBodyScroll"
-//       : "enableBodyScroll";
-//     bodyScrollLock[scrollLockMethod](document.body);
-//   };
-
-//   openMenuBtn.addEventListener("click", toggleMenu);
-//   closeMenuBtn.addEventListener("click", toggleMenu);
-
-//   // Close the mobile menu on wider screens if the device orientation changes
-//   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
-//     if (!e.matches) return;
-//     mobileMenu.classList.remove("is-open");
-//     openMenuBtn.setAttribute("aria-expanded", false);
-//     bodyScrollLock.enableBodyScroll(document.body);
-//   });
-// })();
